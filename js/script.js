7627ba2f6e9ea199abe80f3810807f4365255478
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-12-06 12:13:01 +0000

    Formatted document at script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function getNumberInfo() {
     // Get the value of the Input Field
     let num = document.getElementById("mynumber").value;
     // Check if input is valid
-    if (isNaN(num) || num.length == 0 || num<2 || num>100 || (num.length>1 && num[0] == "0") || !Number.isInteger(Number(num))) {
+    if (isNaN(num) || num.length == 0 || num < 2 || num > 100 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
         txt += `Invalid Input.  Please enter a whole number between 2 and 100.  Do not include leading zeros.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;
@@ -36,17 +36,17 @@ function convergentsOfE(n) {
         return parseInt(sum);
     }
 
-    let convergents = [[2n,1n], [3n,1n]];
-    const multipliers = [1n,1n,2n];
-    for (let i=2;i<n;i++) {
+    let convergents = [[2n, 1n], [3n, 1n]];
+    const multipliers = [1n, 1n, 2n];
+    for (let i = 2; i < n; i++) {
         const [secondLastConvergent, lastConvergent] = convergents;
         const [secondLastNumerator, secondLastDenominator] = secondLastConvergent;
         const [lastNumerator, lastDenominator] = lastConvergent;
-        const curMultiplier = multipliers[i%3];
+        const curMultiplier = multipliers[i % 3];
         const numerator = secondLastNumerator + curMultiplier * lastNumerator;
         const denominator = secondLastDenominator + curMultiplier * lastDenominator;
         convergents = [lastConvergent, [numerator, denominator]];
-        if (i%3===2) multipliers[2] += 2n;
+        if (i % 3 === 2) multipliers[2] += 2n;
     }
     return sumDigits(convergents[1][0]);
 }
